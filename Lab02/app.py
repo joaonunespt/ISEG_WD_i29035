13b41c04ecea29dda3553d8ef96a4696e4a26961
João Nunes <joaonunesptgmail.com>
2023-02-05 11:58:13 +0000

    Lab2 code changes

--- Lab02/app.py
+++ Lab02/app.py
@@ -1,23 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 from flask import request
-from flask import make_response
 
 app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     response = make_response('Hello World!')
+#     response.headers['Content-Type'] = 'text/plain'
+#     return response
 
 @app.route('/')
 def index():
     name = 'John'
-    return render_template('index.html', name = name)
+    return render_template('index.html', name=name)
 
 @app.route('/users/<username>')
 def show_user(username):
     return f'User: {username}'
 
-@app.route('/login', methods=['POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    username = request.form['username']
-    password = request.form['password']
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+            
+        if username == 'test' and password == 'test':
+            return 'Login successful'
+        else:
+            return 'Invalid username or password'
+    else:
+        return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
     
